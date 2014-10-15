b31e3df427529dab356ed89cf507da0ab2ca78ae
Bostan Laura Ana Maria <sarnthil@gmail.com>
2014-10-15 14:50:06 +0300

    L1

---
+++ L1/first.pl
@@ -0,0 +1,28 @@
+print "hello world!\n";
+print "hello world!\n";
+$name = shift;
+$years = shift;
+print "$name is $years\n";
+$i=1;
+$j=$i+1;
+print "$j\n";
+$i=1;
+$i+=1;
+print "$i\n";
+
+$x=shift;
+$y=shift;
+$z=$x*$y;
+print "$x\n";
+print "$y\n";
+print "$z\n";
+
+$u = shift;
+if ($u>3) {
+	print "$u\n";
+	}
+else {
+	print "try again!\n";
+	}
+		
+	
